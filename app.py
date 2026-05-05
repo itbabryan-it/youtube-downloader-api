893c773d62345c2fff65ccdd963cfe45c26a79f7
itbabryan-it <itbabryan@gmail.com>
2026-05-05 12:29:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def download():
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
